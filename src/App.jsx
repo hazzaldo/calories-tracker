5e3dba3c9ebcb512fafda185717c8b4884f87d8a
hazzaldo <hazzaldo@hotmail.com>
2020-06-18 18:37:40 +0100

    import EditMealLog component & add it to route mapping in router + change create route to meal

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MealLogs from './components/MealLogs';
 import CreateUser from './components/CreateUser';
-import CreateMealLog from './components/EditMealLog';
+import CreateMealLog from './components/CreateMealLog';
+import EditMealLog from './components/EditMealLog';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navbar />
       <Route path="/" exact component={MealLogs} />
       <Route path="/user" component={CreateUser} />
-      <Route path="/create" component={CreateMealLog} />
+      <Route path="/meal" component={CreateMealLog} />
+      <Route path="/edit:id" component={EditMealLog} />
     </Router>
   );
 }
