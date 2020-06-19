74813dd2db74fccce0086d493b741af48f13b3b8
hazzaldo <hazzaldo@hotmail.com>
2020-06-19 08:41:16 +0100

    create CreateUser component's basic structure without inclusion of network requests yet

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateUser() {
+
+    const [ user, setUser ] = useState({
+        username: ""
+    });
+
+    function handleChange(event) {
+        const { value } = event.target;
+
+        setUser({
+            username: value
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const newUser = {
+            username: user.username,
+        }
+
+        console.log(newUser);
+
+        setUser({
+            username: ""
+        });
+    }
+
     return (
-        <div>
-            <p>You're in Create User</p>
+        <div className='container form-container'>
+            <h4>ADD A NEW USERNAME</h4>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <input className="form-control" type="text" onChange={handleChange} required name="username" value={user.username} placeholder="Enter a new username"/>
+                </div>
+
+                <div className="form-group">
+                    <button className="btn btn-primary" type="submit">Add username</button>
+                </div>
+            </form>
         </div>
     )
 }
