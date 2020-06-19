6654d8807a9fb7e1aee78ab638a1da34b803d3b4
hazzaldo <hazzaldo@hotmail.com>
2020-06-19 11:12:24 +0100

    complete MeaLogs component structure

--- src/components/MealLogs.jsx
+++ src/components/MealLogs.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MealLog from './MealLog';
 
 function MealLogs() {
+
+    const [mealLogs, setMealLogs] = useState([]);
+
+    useEffect(() => {
+
+        (async ()=> {
+            try {
+                const response = await axios.get('http://localhost:4000/meals');
+                if (response.data.length > 0) {
+                    setMealLogs(response.data);
+                }
+            } catch (err) {
+                console.log(`Error loading meal logs: ${err}`);
+            } 
+        })();
+    }, []);
+
+    async function deleteMealLog(id) {
+        try {
+            const response = await axios.delete('http://localhost:4000/meals'+id);
+            console.log(response.data);
+            setMealLogs(
+                mealLogs.filter(mealLog => mealLog._id !== id)
+            )
+        } catch (err) {
+            console.log(`Error deleting meal log: ${err}`);
+        }
+    }
+
+    function displayMealLogs() {
+        return mealLogs.map(currentMealLog => {
+            return <MealLog 
+            mealLog={currentMealLog}
+            deleteMealLog={deleteMealLog}
+            key={currentMealLog._id}/>
+        })
+    }
+
     return (
-        <div>
-            <p>You're in All Meal Logs</p>
+        <div className="logs-container">
+            <h4>ALL MEAL LOGS</h4>
+            <table className="table">
+                <thead className="thead-light">
+                    <tr>
+                        <th>Username</th>
+                        <th>Description</th>
+                        <th>Calories</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { displayMealLogs() }
+                </tbody>
+            </table>
         </div>
     )
 }
